08703034b6fc3075e6e7625eff46b5ec40d38f27
Ignas Šed <ignas.seduikis@gmail.com>
2022-05-16 11:39:08 +0300

    Student model, timestamps disabled, created fillable, relation to group

---
+++ app/Models/Student.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+      'first_name',
+      'last_name',
+      'group_id'
+    ];
+
+    public function group()
+    {
+      return $this->belongsTo(Group::class);
+    }
+}
