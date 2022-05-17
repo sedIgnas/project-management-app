5fd2a9af44f7753c6fc0cd8d010ce2177c21391e
Ignas Šed <ignas.seduikis@gmail.com>
2022-05-17 22:25:27 +0300

    added fillables to Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,11 +6,18 @@ use App\Models\Group;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+
 class Project extends Model
 {
     use HasFactory;
 
     public $timestamps = false;
+
+    protected $fillable = [
+      'name',
+      'group_size',
+      'group_count'
+    ];
 
     public function groups()
     {
