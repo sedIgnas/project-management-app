d2763bf0e64a5b70ecd093e3c6a9073f6a01eb87
Ignas Šed <ignas.seduikis@gmail.com>
2022-05-17 22:20:20 +0300

    added api route for students with resources, student route for displaying data

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,14 @@
 
 use App\Http\Controllers\Main\HomeController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\StudentController;
 use App\Models\Project;
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/project', [ProjectController::class, 'index'])->name('project');
+Route::post('/project', [ProjectController::class, 'store'])->name('project.store');
+Route::get('/project', [ProjectController::class, 'index'])->name('project.index');
+Route::resource('student', StudentController::class);
 
