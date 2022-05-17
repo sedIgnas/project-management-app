6998573fbf4adafff63cf75f9b4cac5441744823
Ignas Šed <ignas.seduikis@gmail.com>
2022-05-17 22:21:32 +0300

    Added forms for student table, groups. Passes data to request

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,23 +22,23 @@
 
     <h2 class="my-5">Create a project first</h1>
 
-      <form class="w-25 mx-auto" action="POST">
-        
+      <form class="w-25 mx-auto" method="POST" action="{{ route('project.store') }}">
+        @csrf
+        @method('POST')
         <div class="form-group">
           <label for="projectName">Project Name</label>
-          <input type="text" class="form-control" id="projectName" placeholder="e.g. Cabin in the woods painting">
+          <input type="text" class="form-control" name="projectName" id="projectName" placeholder="E.g. World population">
         </div>
         <div class="form-group">
-          <label for="groupNumber">Number of groups</label>
-          <input type="text" class="form-control" id="groupNumber" placeholder="e.g. 4">
+          <label for="groupCount">Number of groups</label>
+          <input type="number" class="form-control" name="groupCount" id="groupCount" placeholder="E.g. 4">
         </div>
         <div class="form-group">
           <label for="groupSize">Maximum students in group</label>
-          <input type="text" class="form-control" id="groupSize" placeholder="e.g. 2">
+          <input type="number" class="form-control" name="groupSize" id="groupSize" placeholder="E.g. 2">
         </div>
-        <button class="btn btn-warning"><a class="text-reset" href="{{ route('project') }}">Create</a></button>
+        <input type="submit" value="Create project" class="btn btn-warning">
       </form>
-
 
     </div>
 </body>
--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -19,17 +19,14 @@
 <body>
   {{-- main page container --}}
   <div class="container">
-    @if($test === true)
-    <form action=""></form>
-    @else
-
+    {{-- @if($isProjAvail === true) --}}
         {{-- Project overview --}}
         <div class="m-5">
             <h4>Project status</h4>
             <div class="ml-3">
-                <p>Project name: <span class="font-weight-bold">{{ $project->first()->name }}</span></p>
-                <p>Number of groups: <span class="font-weight-bold">{{ $groupCount }}</span></p>
-                <p>Students per group: <span class="font-weight-bold">--from-input--</span></p>
+                <p>Project name: <span class="font-weight-bold">{{ $project->name }}</span></p>
+                <p>Number of groups: <span class="font-weight-bold">{{ $project->group_count }}</span></p>
+                <p>Students per group: <span class="font-weight-bold">{{ $project->group_size }}</span></p>
             </div>
         </div>
 
@@ -54,27 +51,59 @@
                             -
                           @endif
                         </td>
-                        <td>Delete</td>
+                        <td>
+                          <form action="{{ route('students.destroy', $student) }}" method="post">
+                            @csrf
+                            @method('delete')
+                            <input type="submit" value="Delete" class="btn btn-light">
+                          </form>
+                        </td>
                     </tr>
                   @endforeach
+                  <tr>
+                    <form action="{{ route('student.store') }}" method="POST">
+                      @csrf
+                      @method('POST')
+                      <td>
+                        <input type="text" name="firstName" id="firstName" placeholder="First name">
+                        <input type="text" name="lastName" id="lastName" placeholder="Last name">
+                      </td>
+                      <td>
+                        <select name="groupId" id="groupId">
+                          <option id="groupId" name="groupId" value="{{ null }}">-</option>
+                          @foreach($groups as $group)
+                          <option id="groupId" name="groupId" value="{{ $group->id }}">{{ $group->name }}</option>
+                          @endforeach
+                        </select>
+                      </td>
+                      <td>
+                        <input type="submit" value="Add new Student" class="btn btn-light">
+                      </td>
+                    </form>
+                  </tr>
                 </tbody>
-            </table>
-            <div class="btn btn-light">Add new student</div>
+              </table>
         </div>
 
         {{-- Student groups --}}
         <div class="row m-auto w-100 text-center">
+
+          @foreach($groups as $group)
             <div class="col-6 mt-5">
                 <table class="table table-bordered">
                     <thead>
                         <tr>
-                            <th>Group #1</th>
+                            <th>{{ $group->name }}</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Student 1</td> 
-                        </tr>
+                      @foreach($students as $student)
+                        @if ($student->group_id === $group->id)
+                            <tr>
+                              <td>{{ $student->name }} {{ $student->last_name }}</td>
+                            </tr>
+                        @endif 
+                        @endforeach
                         <tr>
                             <td>
                                 <select class="custom-select">
@@ -88,51 +117,14 @@
                     </tbody>
                 </table>
             </div>
-
-            <div class="col-6 mt-5">
-              <table class="table table-bordered">
-                  <thead>
-                      <tr>
-                          <th>Group #1</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <tr>
-                          <td>Student 1</td>
-                      </tr>
-                      <tr>
-                          <td>Student 2</td>
-                      </tr>
-                  </tbody>
-              </table>
-          </div>
-
-          <div class="col-6 mt-5">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Group #1</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Student 1</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <select class="custom-select">
-                                <option selected>Assign student</option>
-                                <option value="1">Student 1</option>
-                                <option value="2">Student 2</option>
-                                <option value="3">Student 3</option>
-                            </select>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-          </div>
+            @endforeach
         </div>
-        @endif
+        {{-- @else
+        <div class="container text-center mt-5 mx-auto">
+          <h1>Please create a project</h1>
+          <a class="btn btn-danger text-reset" href="{{ route('home') }}">Create project now</a>
+        </div>
+        @endif --}}
     </div>
 </body>
 
