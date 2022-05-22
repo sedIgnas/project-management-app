eb6e1e4c73a3765b83730d7171c0ea8df0bbf17e
Ignas Šed <ignas.seduikis@gmail.com>
2022-05-23 01:29:45 +0300

    removed constraint on group_id, from students

--- database/migrations/2022_05_15_100003_create_students_table.php
+++ database/migrations/2022_05_15_100003_create_students_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->boolean('in_group');
-            $table->foreignId('group_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('group_id')->nullable()->onDelete('cascade');
         });
     }
 
