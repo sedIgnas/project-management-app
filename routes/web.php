1a01b1a8cee4ee9631169b5321a5843548b0249f
Ignas Šed <ignas.seduikis@gmail.com>
2022-05-23 04:51:01 +0300

    updated routes, deleted unused

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,10 @@
 
 use App\Http\Controllers\Main\HomeController;
 use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\StudentController;
-use App\Models\Project;
-use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::post('/project', [ProjectController::class, 'store'])->name('project.store');
 Route::get('/project', [ProjectController::class, 'index'])->name('project.index');
-Route::resource('student', StudentController::class);
 
