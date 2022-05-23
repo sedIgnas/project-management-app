bb25238c4d88ff0c4ee6e859d065baa02640a6eb
Ignas Šed <ignas.seduikis@gmail.com>
2022-05-23 05:08:25 +0300

    added construct method, moved store to here, from projectController

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -3,91 +3,89 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreStudentRequest;
 use App\Http\Resources\StudentResource;
 use App\Models\Student;
+use App\Repositories\Student\StudentRepository;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $students = Student::all();
-        return StudentResource::collection($students);
+  private Student $student;
+  private StudentRepository $studentRepository;
+
+  public function __construct(Student $student, StudentRepository $studentRepository)
+  {
+    $this->student = $student;
+    $this->studentRepository = $studentRepository;
+  }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $students = Student::all();
+    return StudentResource::collection($students);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(StoreStudentRequest $request)
+  {
+    $inGroup = $request->input('groupId') ? 1 : 0;
+
+    $this->student->first_name = $request->input('firstName');
+    $this->student->last_name = $request->input('lastName');
+    $this->student->in_group = $inGroup;
+    $this->student->group_id = $request->input('groupId');
+
+    if (!$this->studentRepository->checkForStudentDuplicate($this->student)) {
+      $this->student->save();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    return redirect(route('project.index'));
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-      $students = Student::create($request->all());
-        
-      return new StudentResource($students);
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Models\Student  $student
+   * @return \Illuminate\Http\Response
+   */
+  public function update(StoreStudentRequest $request)
+  {
+    $studentid = $request->input('id');
+    $student = $this->student->where('id', $studentid);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Student  $student
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Student $student)
-    {
-        //
-    }
+    $groupId = $request->input('groupId');
+    $inGroup = $groupId ? 1 : 0;
+    $student->in_group = $inGroup;
+    $student->group_id = $request->input('groupId');
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Student  $student
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Student $student)
-    {
-        //
-    }
+    $student->update([
+      'in_group' => $student->in_group,
+      'group_id' => $groupId
+    ]);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Student  $student
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Student $student)
-    {
-      $student->update($request->all());
+    return redirect(route('project.index'));
+  }
 
-      return new StudentResource($student);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Student  $student
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Student $student)
-    {
-      $student->delete();
-      return redirect(route('project.index'));
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\Student  $student
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Student $student)
+  {
+    $student->delete();
+    return redirect(route('project.index'));
+  }
 }
